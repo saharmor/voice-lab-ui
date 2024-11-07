7f1ab7fa3a5f65fa1f07fda86e1684cbe81b96a7
Sahar Mor <saharhashai@gmail.com>
2024-11-07 14:08:41 -0800

    gh pages fix

--- json-config-editor/vite.config.js
+++ json-config-editor/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/json-config-editor/',
+  base: '/voice-lab-ui/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
